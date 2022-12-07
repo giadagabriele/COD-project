9a8b6c836321392d0127a82cf7b11002d25d6d08
Giada Gabriele <giadagabriele@protonmail.com>
2022-12-07 21:37:45 +0100

    aggiunto un minimo di css

--- backend/backend.py
+++ backend/backend.py
@@ -51,17 +51,17 @@ def home():
         <head>
             <title>File Upload</title>
         </head>
-        <body>
-            <h1>Upload your File</h1>
-            <form action="/upload" method="POST" enctype="multipart/form-data">
+        <body style="max-width: max-content; margin: auto; margin-top: 100px">
+            <h1 style="color: red; font-family: Arial, Helvetica, sans-serif;">Upload your File</h1>
+            <form method="POST" enctype="multipart/form-data">
                 <input type="file" name="file">
-                <input type="submit" value="Upload">
+                <br><br>
+                <input style="height:40px; width:250px;" type="submit" value="Upload">
+                <br><br><br>
+                <hr>
+                <br><br>
+                <a style="color: black;" href="http://127.0.0.1:5000/images"><button style="height:50px; width:250px;" type="button">List of images already uploaded</button></a>
             </form>
         </body>
     </html>
     """
-
-
-# SOLUTION:
-
-# The payload could be a malicious file with a filename that has an allowed extension, such as "file.png.exe", which would bypass the extension filter.
